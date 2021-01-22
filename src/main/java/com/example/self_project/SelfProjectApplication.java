d014b35d69ae7c57b89f33b898ec9b0d8445f202
mryed <mryediniofficial9924@gmail.com>
2021-01-23 00:49:34 +0500

    1wew

--- src/main/java/com/example/self_project/SelfProjectApplication.java
+++ src/main/java/com/example/self_project/SelfProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SelfProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SelfProjectApplication.class, args);
 	}
